167111a2fab4e1075da0b24dc6e8dd3ac1712468
Julio Vieira <juliostefano1327@gmail.com>
2024-08-01 20:17:07 -0300

    PROCEDURE CRIADA - Avançada

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -1,6 +1,5 @@
 -- PROCEDURE
-
---
+/*
 CREATE PROCEDURE prOrderGerente
 AS
 BEGIN
@@ -26,3 +25,41 @@ BEGIN
 END 
 
 EXECUTE prListarClientes @ano = 1989
+
+*/
+
+CREATE OR ALTER PROCEDURE prRegistraContrato(@gerente VARCHAR(MAX), @cliente VARCHAR(MAX), @valor FLOAT)
+AS
+BEGIN
+	DECLARE 
+		@vIDgerente INT, 
+		@vIDcliente INT
+		
+	SELECT
+		@vIDgerente = id_gerente
+	FROM
+		dGerente
+	WHERE nome_gerente = @gerente
+
+	SELECT 
+		@vIDcliente = id_cliente
+	FROM
+		dCliente
+	WHERE nome_cliente = @cliente
+
+	INSERT INTO fContratos(data_assinatura, id_cliente, id_gerente , valor_contrato)
+	VALUES
+		(GETDATE(), @vIDcliente, @vIDgerente, @valor)
+		
+	PRINT 'PROCEDURE EXECUTADA COM SUCESSO '
+
+
+END
+
+
+EXECUTE prRegistraContrato @gerente = 'Lucas Sampaio', @cliente =  'Gustavo Barbosa' , @valor = 5000
+
+SELECT 
+	*
+FROM
+	fContratos
